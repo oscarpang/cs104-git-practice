f45825ae4b7e418c7e443c3c8cdbe34d16ce433c
Weiwu Pang <weiwupan@usc.edu>
2018-01-12 13:18:38 -0800

    add two lines

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,4 +4,6 @@ int main()
 {
   std::cout << "HelloWorld!" << std::endl;
   std::cout << "CS104" << std::endl;
+  std::cout << "is a great course" << std::endl;
+  std::cout << "Really?" << std::endl;
 }
